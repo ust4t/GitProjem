0842ae5e0e57b202d1a16c9e22d9f55ac89b33b6
Yasin Halit Karayağız <ustat@hotmail.it>
2023-03-16 20:21:09 +0300

    Initial Commit

---
+++ GitProjem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitProjem
+//
+//  Created by krygz on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
